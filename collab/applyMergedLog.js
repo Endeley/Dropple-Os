aa52650cad91aee17f56183b1a694c42488b61ec
Endeley <endeleygold@hotmail.com>
2026-04-08 20:20:57 +0200

    refactor(runtime): remove stale branch and remote sync leftovers

--- collab/applyMergedLog.js
+++
@@ -1,8 +0,0 @@
-export function applyMergedLog(events, dispatcher) {
-    events.forEach((evt) => {
-        dispatcher.dispatch({
-            type: evt.type,
-            payload: evt.payload,
-        });
-    });
-}
